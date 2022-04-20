90ca5f5a845439192a378d4358bd5659c697b327
Davitkhachikyan <davitkhachikyan93@gmail.com>
2022-04-20 18:38:29 +0400

    updated

--- app/views/add_product.php
+++ app/views/add_product.php
@@ -6,7 +6,7 @@
 
     <input type="file" name="file" id="fileToUpload"><br><br>
     <input type="text" name="description">description <br><br>
-    <input type="text" name="name"><br><br>
+    <input type="text" name="name">name<br><br>
     <input type="submit" value="Upload Image" name="submit"> <br><br>
 
 </form>
